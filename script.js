4973c7a6fd881639ec89096b85f80d1bf11cf9f9
unknown <husnulmuawiahpasehai.com>
2026-09-25 18:07:25 +0800

    Update portfolio

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 if (document.title === "Husnul — Home") {
-
     let intro = document.createElement("div");
-
     intro.innerHTML = `
         <div style="
             position: fixed;
@@ -15,21 +13,8 @@ if (document.title === "Husnul — Home") {
             text-align: center;
             z-index: 9999;
         ">
-
-            <h1 style="
-                font-size: 48px;
-                margin-bottom: 10px;
-            ">
-                Halooo
-            </h1>
-
-            <p style="
-                font-size: 20px;
-                margin-bottom: 30px;
-            ">
-                Selamat Datang Di Portfolio Husnul
-            </p>
-
+            <h1 style="font-size: 48px; margin-bottom: 10px;">Halooo Teman Teman </h1>
+            <p style="font-size: 20px; margin-bottom: 30px;">Selamat Datang Di Portofolio Husnul </p>
             <button id="masuk" style="
                 padding: 12px 35px;
                 border: none;
@@ -38,17 +23,11 @@ if (document.title === "Husnul — Home") {
                 color: #10243E;
                 font-size: 16px;
                 cursor: pointer;
-            ">
-                MASUK
-            </button>
-
+            ">MASUK</button>
         </div>
     `;
-
     document.body.appendChild(intro);
-
     document.getElementById("masuk").addEventListener("click", function() {
         intro.remove();
     });
-
 }
